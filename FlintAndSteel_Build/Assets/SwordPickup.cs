e402d34a663d312f950e695e57277cd1425f3cfc
evankeyes <ekeyes1@ncstudents.niagaracollege.ca>
2024-02-23 17:01:46 -0500

    Updated the place you find Steel

--- FlintAndSteel_Build/Assets/SwordPickup.cs
+++ FlintAndSteel_Build/Assets/SwordPickup.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class SwordPickup : MonoBehaviour
 {
+    bool inRange;
+    GameObject playerGO;
+
     private void Start()
     {
         gameObject.SetActive(true);
     }
 
-    private void OnTriggerStay(Collider other)
+    private void Update()
     {
         // If the player is in range and right clicks, he picked up Steel!
+        if (Input.GetMouseButtonDown(0) && inRange)
+        {
+            playerGO.gameObject.GetComponent<PlayerAttackScript>().steelFound = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetMouseButtonDown(1))
-            {
-                other.gameObject.GetComponent<PlayerAttackScript>().steelFound = true;
-                gameObject.SetActive(false);
-            }
+            inRange = true;
+            playerGO = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
         }
     }
 }
