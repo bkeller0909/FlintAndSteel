3f0f51aae3d3a6093e7591c905a775a2e3658acb
amathew42 <amathew42@ncstudents.niagaracollege.ca>
2024-03-07 12:43:41 -0500

    Modified the Recall sword to take in the teleport shader
    It kind of works but still got stuff to work on

--- FlintAndSteel_Build/Assets/Game/Scripts/Player/PlayerAttackScript.cs
+++ FlintAndSteel_Build/Assets/Game/Scripts/Player/PlayerAttackScript.cs
@@ -1,38 +1,30 @@
-//------------------------------------------------------------------
-//Scriptfor the sword throw aiming and recalling the sword
-//Attach this script to the player and tag the object that needs to be thrown as Sword
-//Check isKinematic and disable gravity
-//ADjust the throw force as you need in the inspector
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerAttackScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] float throwForce = 10.0f;
     [SerializeField] GameObject sword;
     private bool isSwordThrown;
     private Rigidbody rb;
     private Collider swordCollider;
-    Camera mainCamera;
+    private Camera mainCamera;
     [SerializeField] private Transform throwPosition;
     public bool steelFound;
 
-    [SerializeField]
-    Renderer[]  swordRenderer;
+    [SerializeField] Renderer[] swordRenderer;
+    Material swordMaterial;
 
-    private float fadeOutValueStart = 0.704f;  //Initial fadeout value
-    private float fadeInValueStart = 0.0f;  //Intial fadein value
-    private float fadeOutSpeed = 5.0f;  //Speed of fadeout
+    private float fadeOutValue = 0f; // Initial fade out value
+    private float fadeOutSpeed = 0.5f; // Speed of fade out
+
     void Start()
     {
         mainCamera = Camera.main;
         rb = sword.GetComponent<Rigidbody>();
         swordCollider = sword.GetComponent<Collider>();
+        
         if (rb != null)
         {
             rb.isKinematic = true;
@@ -41,19 +33,18 @@ public class PlayerAttackScript : MonoBehaviour
         {
             Debug.LogError("Rigidbody not found on the sword GameObject.");
         }
+        
         swordCollider.enabled = false;
 
-
-        SetFadeOutValue(fadeOutValueStart);
-        
+        swordMaterial = swordRenderer[0].material;
+        SetFadeOutValue(0f); // Set initial fade out value
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (steelFound)
         {
-            if (Input.GetMouseButtonDown(0) && GetComponent<Throwing>().pulling == false)
+            if (Input.GetMouseButtonDown(0) && !GetComponent<Throwing>().pulling)
             {
                 if (isSwordThrown)
                 {
@@ -65,71 +56,51 @@ public class PlayerAttackScript : MonoBehaviour
                 }
             }
         }
-        Debug.Log("Fade out occurs");
+
         UpdateSteelRenderer();
     }
 
     private void UpdateSteelRenderer()
     {
-        //hide the sword if the player hasnt picked it up yet
         if (!steelFound && sword != null)
         {
-            //Hide all renderers
-            Renderer[] swordPieces = sword.GetComponentsInChildren<Renderer>();
-
-            for (int i = 0; i < swordPieces.Length; i++)
-            {
-                swordPieces[i].enabled = false;
-            }
+            SetFadeOutValue(0f); // Hide the sword if the player hasn't picked it up yet
         }
         else
         {
-            //Show all renderers
-            Renderer[] swordPieces = sword.GetComponentsInChildren<Renderer>();
-
-            for (int i = 0; i < swordPieces.Length; i++)
-            {
-                swordPieces[i].enabled = true;
-            }
+            SetFadeOutValue(1f); // Show the sword if the player has picked it up
         }
     }
 
     void AimAndThrow()
     {
-        //Getting the mouse position
         Vector3 mousePosition = Input.mousePosition;
- 
-        
         Ray rayCast = mainCamera.ScreenPointToRay(mousePosition);
 
-        if (Physics.Raycast(rayCast, out RaycastHit hitInfo, Mathf.Infinity)) 
+        if (Physics.Raycast(rayCast, out RaycastHit hitInfo, Mathf.Infinity))
         {
-            //Setting a min distance
             float minThrowDistance = 1f;
             
-            //Check to make sure the mouse is clicked a distance away from the player
             if (Vector3.Distance(hitInfo.point, transform.position) > minThrowDistance)
             {
                 Vector3 direction = hitInfo.point - transform.position;
                 direction.Normalize();
                 direction.z = 0;
 
-                //After getting the position of the mouse it calls the throw sword function to the trow the sword
                 ThrowSword(direction);
             }
-
             else
             {
-                Debug.Log("Mouse click too close to the character. Sword mnot thrown");
+                Debug.Log("Mouse click too close to the character. Sword not thrown");
             }
         }
     }
+
     void ThrowSword(Vector3 direction)
     {
         Quaternion rotation = Quaternion.LookRotation(direction);
-        //Sets the rotation in real time right before the sword is thrown
-        rotation *= Quaternion.Euler(90,0, 0);
-        GameObject thrownSword = Instantiate(sword, throwPosition.position, rotation);// Quaternion.LookRotation(direction));
+        rotation *= Quaternion.Euler(90, 0, 0);
+        GameObject thrownSword = Instantiate(sword, throwPosition.position, rotation);
         rb = thrownSword.GetComponent<Rigidbody>();
         thrownSword.GetComponent<Collider>().enabled = true;
 
@@ -138,55 +109,41 @@ public class PlayerAttackScript : MonoBehaviour
             sword.SetActive(false);
             rb.isKinematic = false;
             isSwordThrown = true;
-        
             rb.AddForce(direction * throwForce, ForceMode.Impulse);
-            Debug.Log("Sword throw script runs");
-
         }
         else
         {
             Debug.LogError("Rigidbody not found on the thrown sword GameObject.");
         }
     }
+
     void SwordRecall()
     {
         swordCollider.enabled = false;
-        StartCoroutine(FadeOutSword());
-
-        sword.SetActive(true);
-        //StartCoroutine(FadeInSword());
-
-        isSwordThrown = false;
-
-        //does the coRoutine thjat does the fadeout sword effect
-       
-    }
-
-    public IEnumerator FadeOutSword()
-    {
-        while (fadeOutValueStart > 0.0f)
-        {
-            
-            fadeOutValueStart -= fadeOutSpeed * Time.deltaTime;
-            SetFadeOutValue(fadeOutValueStart);
-            yield return null;
-
-
-        }
-        Destroy(GameObject.FindWithTag("Sword")); // Destroy the thrown sword 
-    }
-
-    public IEnumerator FadeInSword()
-    {
-        while (fadeOutValueStart < 0.0f)
-        {
-            fadeOutValueStart += fadeOutSpeed * Time.deltaTime;
-            SetFadeOutValue(fadeOutValueStart);
-            yield return null;
-
-        }
+        Destroy(GameObject.FindWithTag("Sword"));
         
+        isSwordThrown = false;
+        
+        // Start fade-out coroutine when sword is recalled
+        StartCoroutine(FadeOutSword());
+        sword.SetActive(true);
+        //Debug.LogWarning(fadeOutValue.ToString());
     }
+
+    // Coroutine to gradually fade out the sword
+   public IEnumerator FadeOutSword()
+    {
+        while (fadeOutValue < 1f)
+        {
+            fadeOutValue += fadeOutSpeed * Time.deltaTime;
+            SetFadeOutValue(fadeOutValue);
+           
+            yield return null;
+        }
+        fadeOutValue = 0;
+    }
+
+    // Function to set the fade out value of the sword material
     void SetFadeOutValue(float value)
     {
         foreach (Renderer renderer in swordRenderer)
@@ -194,5 +151,4 @@ public class PlayerAttackScript : MonoBehaviour
             renderer.material.SetFloat("_FadeOut", value);
         }
     }
-
 }
