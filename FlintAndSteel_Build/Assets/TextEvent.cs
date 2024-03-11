418cdb8c4e1984c71516f8d59e03db81dd85cc3e
mkeyes21 <158609582+mkeyes21@users.noreply.github.com>
2024-03-10 21:06:58 -0400

    Dialogue

--- FlintAndSteel_Build/Assets/TextEvent.cs
+++ FlintAndSteel_Build/Assets/TextEvent.cs
@@ -36,7 +36,7 @@ public class TextEvent : MonoBehaviour
         messageGO.SetActive(messageActive);
 
         // If the player presses any key, exit the message
-        if (messageActive && Input.anyKeyDown) 
+        if (messageActive && Input.GetButtonDown("LeftShift")); 
         {
             NextMessage();
         }
