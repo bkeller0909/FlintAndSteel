554a5c28a8ba2060b7b54a26904d0f67a17799a9
mkeyes21 <158609582+mkeyes21@users.noreply.github.com>
2024-03-19 16:11:40 -0400

    Update Seagull.cs

--- FlintAndSteel_Build/Assets/Seagull.cs
+++ FlintAndSteel_Build/Assets/Seagull.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 
 public class Seagull : MonoBehaviour
 {
+
+    private float startDelay;
+    private float delayTimer;
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        startDelay = UnityEngine.Random.Range(0.01f, 2.00f);
+        delayTimer = startDelay;
     }
+    private void Update()
+    {
+        delayTimer -= Time.deltaTime;
+        if (delayTimer <= 0) 
+        {
+            animator.SetBool("Begin", true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
