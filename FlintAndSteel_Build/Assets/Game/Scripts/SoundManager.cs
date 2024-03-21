cd7263edfc15cb45e97140b96cf538e70cbca4c4
Brandon Keller <bkeller0909@gmail.com>
2024-03-21 14:53:40 -0400

    Sound manager script and mixer

---
+++ FlintAndSteel_Build/Assets/Game/Scripts/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField] private AudioSource soundObject;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        // spawn in the sound object
+        AudioSource audioSource = Instantiate(soundObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayRandomSoundClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    {
+        // assign index value
+        int randClip = Random.Range(0, audioClip.Length);
+
+        // spawn in the sound object
+        AudioSource audioSource = Instantiate(soundObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip[randClip]; // changed to an array so it will grab a random clip
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
