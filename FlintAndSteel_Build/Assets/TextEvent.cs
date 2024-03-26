646e60a8d4ec5f435bd900fdeca84a6c1b57bcc8
Brandon Keller <bkeller0909@gmail.com>
2024-03-26 18:09:52 -0400

    Changed button press for text event
    Controller

--- FlintAndSteel_Build/Assets/TextEvent.cs
+++ FlintAndSteel_Build/Assets/TextEvent.cs
@@ -36,7 +36,7 @@ public class TextEvent : MonoBehaviour
         messageGO.SetActive(messageActive);
 
         // If the player presses any key, exit the message
-        if (messageActive && Input.GetKeyDown(KeyCode.E) || messageActive && Input.GetButtonDown("Jump"))
+        if (messageActive && Input.GetKeyDown(KeyCode.E) || messageActive && Input.GetButtonDown("Dialogue Skip"))
         {
             NextMessage();
         }
