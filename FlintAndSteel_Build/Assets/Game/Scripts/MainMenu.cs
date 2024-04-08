529b64fe425e336f4c81f94771168b21e35d680b
Brandon Keller <bkeller0909@gmail.com>
2024-04-08 03:05:16 -0400

    Main Menu
    controller can be toggled on, at the main menu

--- FlintAndSteel_Build/Assets/Game/Scripts/MainMenu.cs
+++ FlintAndSteel_Build/Assets/Game/Scripts/MainMenu.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject FadeOut;
+
+    [Header("Controller")]
+    [SerializeField] Toggle controllerToggle;
+    public bool controllerON = false;
+
+    private void Awake()
+    {
+        if (GameManager.Instance.usingController == true)
+        {
+            controllerToggle.isOn = true;
+            controllerON = true;
+        }
+    }
 
     public void PlayGame()
     {
@@ -23,5 +37,19 @@ public class MainMenu : MonoBehaviour
         GameManager.Instance.coinAmount = 0;
         GameManager.Instance.coinsAtLevelStart = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Controller(bool isController)
+    {
+        controllerON = isController;
+
+        if (controllerON)
+        {
+            GameManager.Instance.usingController = true;
+        }
+        else
+        {
+            GameManager.Instance.usingController = false;
+        }
     }
 }
