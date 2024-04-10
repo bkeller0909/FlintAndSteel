bc306c2775e1d978b7c34ea5c92f67f8c93389f9
Brandon Keller <bkeller0909@gmail.com>
2024-04-10 12:36:16 -0400

    Story Intro text scaling

--- FlintAndSteel_Build/Assets/SkipCutscene.cs
+++ FlintAndSteel_Build/Assets/SkipCutscene.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SkipCutscene : MonoBehaviour
 {
     MainMenu menuScript;
+    [SerializeField] GameObject skipText;
 
     private void Awake()
     {
@@ -16,6 +18,15 @@ public class SkipCutscene : MonoBehaviour
         if (Input.GetButtonDown("Dialogue Skip") || Input.GetKeyDown(KeyCode.E))
         {
             menuScript.LoadLevel();
+        }
+
+        if(GameManager.Instance.usingController == true)
+        {
+            skipText.GetComponent<TextMeshProUGUI>().text = "Skip: SQR";
+        }
+        else
+        {
+            skipText.GetComponent<TextMeshProUGUI>().text = "Skip: E";
         }
     }
 }
